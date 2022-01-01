29e2f73cae084e947f3dca998cfec41cd090ac35
takurinton <takurinton@takurinton.com>
2022-01-01 14:13:46 +0900

    add: drawing box

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,52 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 export const App = () => {
-  return <Box>hoge</Box>;
+  return (
+    <>
+      <DrawingModal />
+    </>
+  );
 };
+
+type Pos = {
+  x: number;
+  y: number;
+}
+
+const DrawingModal = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [mousedownPosition, setMousedownPosition] = useState<Pos>({ x: 0, y: 0 });
+  const [mouseupPosition, setMouseupPosition] = useState<Pos>({ x: 0, y: 0 });
+  const [children, setChildren] = useState<JSX.Element[]>([]);
+
+  const mousedownFunction = useCallback((event) => {
+    setMousedownPosition({ x: event.pageX, y: event.pageY });
+  }, [mousedownPosition, mouseupPosition]);
+
+  const mouseupFunction = useCallback((event) => {
+    setMouseupPosition({ x: event.pageX, y: event.pageY });
+    const height = Math.abs(mousedownPosition.y - event.pageY);
+    const width = Math.abs(mousedownPosition.x - event.pageX);
+    const left = event.pageX > mousedownPosition.x ? mousedownPosition.x : event.pageX;
+    const top = event.pageY > mousedownPosition.y ? mousedownPosition.y : event.pageY;
+    const cihld = <Box h={height} w={width} top={top} left={left} position='absolute' border='1px solid black' backgroundColor='white' />
+    setChildren([...children, cihld]);
+  }, [mousedownPosition, mouseupPosition]);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", mousedownFunction, false);
+    return () => document.removeEventListener("mousedown", mousedownFunction, false)
+  }, [mousedownPosition, mouseupPosition]);
+
+  useEffect(() => {
+    document.addEventListener("mouseup", mouseupFunction, false);
+    return () => document.removeEventListener("mouseup", mouseupFunction, false)
+  }, [mousedownPosition, mouseupPosition]);
+
+  return (
+    <Box position='relative' ref={ref}>
+      {children.map((child, idx) => <React.Fragment key={idx.toString()}>{child}</React.Fragment>)}
+    </Box>
+  );
+}
