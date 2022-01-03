aed27794a6a322da47d8bff800417cbb7412c776
takurinton <takurinton@takurinton.com>
2022-01-03 10:07:12 +0900

    add: children judge

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,11 @@ type Pos = {
   y: number;
 }
 
-type DrawingState = 'draw' | 'resize' | 'drag' | 'drop' | 'inputText' | 'arrow' | 'nothing';
+type DrawingState = 'draw' | 'resize' | 'move' | 'inputText' | 'arrow' | 'nothing';
 const tabs: DrawingState[] = [
   'draw',
   'resize',
-  'drag',
-  'drop',
+  'move',
   'inputText',
   'arrow',
   'nothing'
@@ -50,11 +49,22 @@ const DrawingModal = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
+  // const parentRef = useRef<HTMLDivElement>(null);
   const ref = useRef<HTMLDivElement>(null);
   const [state, setState] = useState<DrawingState>('nothing');
   const [mousedownPosition, setMousedownPosition] = useState<Pos>({ x: 0, y: 0 });
   const [mouseupPosition, setMouseupPosition] = useState<Pos>({ x: 0, y: 0 });
   const [contents, setContents] = useState<JSX.Element[]>([]);
+
+  const onDragStart = useCallback((event) => {
+    event.dataTransfer.setData("text", event.target.id);
+  }, []);
+
+  const onDrop = useCallback((event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+  }, []);
 
   const mousedownFunction = useCallback((event) => {
     setMousedownPosition({ x: event.pageX, y: event.pageY });
@@ -66,9 +76,13 @@ const DrawingModal = ({
     const width = Math.abs(mousedownPosition.x - event.pageX);
     const left = event.pageX > mousedownPosition.x ? mousedownPosition.x : event.pageX;
     const top = event.pageY > mousedownPosition.y ? mousedownPosition.y : event.pageY;
-    const cihld = <Box h={height} w={width} top={top} left={left} position='absolute' border='1px solid black' />
+    const cihld = <Box h={height} w={width} top={top} left={left} position='absolute' border='1px solid black' draggable={true} id={`${top}-${left}`} onDragStart={onDragStart} />
     setContents([...contents, cihld]);
   }, [mousedownPosition, mouseupPosition]);
+
+  const mousemoveFunction = useCallback((event) => {
+    console.log(event.target.id);
+  }, []);
 
   useEffect(() => {
     if (state === 'draw') {
@@ -84,6 +98,15 @@ const DrawingModal = ({
     }
   }, [state, mousedownPosition, mouseupPosition]);
 
+  useEffect(() => {
+    if (state === 'move') {
+      Array.prototype.forEach.call(ref.current?.children, target => {
+        target.addEventListener("mousedown", mousemoveFunction, false);
+        return () => target.removeEventListener("mousedown", mousemoveFunction, false);
+      });
+    }
+  }, [state]);
+
   return (
     <Box position='relative'>
       <Modal isOpen={isOpen} onClose={onClose} size='full'>
@@ -92,7 +115,7 @@ const DrawingModal = ({
           <ModalHeader>drawing</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Tabs onChange={(idx) => setState(tabs[idx])} defaultIndex={6}>
+            <Tabs onChange={(idx) => setState(tabs[idx])} defaultIndex={5}>
               <TabList>
                 {tabs.map((d, i) => <Tab key={i.toString()}>{d}</Tab>)}
               </TabList>
@@ -100,7 +123,7 @@ const DrawingModal = ({
                 {tabs.map((d, i) => <TabPanel key={i.toString()}>{d}</TabPanel>)}
               </TabPanels>
             </Tabs>
-            <Box ref={ref} h='600px'>
+            <Box ref={ref} h='600px' dropzone='move' onDrop={onDrop}>
               {contents.map((child, idx) => <React.Fragment key={idx.toString()}>{child}</React.Fragment>)}
             </Box>
           </ModalBody>
